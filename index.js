30fbca061e831e6a3639566318965478a97e9dda
Temple <tozoaz@gmail.com>
2023-07-14 11:05:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ app.post('/create', async (req, res) => {
   });
   console.log(response.data);
   if (response.data.choices[0].text) {
+    res.header('Access-Control-Allow-Origin', 'https://gleeful-tulumba-524329.netlify.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.json({
       message: response.data.choices[0].text,
     });
