6aea2438dd383ec115c7111d727e54325acaf3ab
Temple <tozoaz@gmail.com>
2023-07-14 09:48:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
 
-// Set up CORS headers
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
 
 
 /* ROUTES */
