454d3af374a7d55c31d0302f9d4b64b1ab3cb197
Temple <tozoaz@gmail.com>
2023-07-14 09:51:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,9 @@ app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
-app.use(cors());
-
-
-
+app.use(cors({
+  origin: 'https://gleeful-tulumba-524329.netlify.app',
+}));
 
 /* ROUTES */
 app.use('/auth', authRoutes);
